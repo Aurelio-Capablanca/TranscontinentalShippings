edebb21c7b5faf9472f07a644ada5325a77fc9af
Aurelio <aurelio.iempereur@gmail.com>
2025-07-17 20:56:39 -0600

    push all

---
+++ src/main/java/com/aib/transcontinentalshippings/TranscontinentalShippingsApplication.java
@@ -0,0 +1,13 @@
+package com.aib.transcontinentalshippings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscontinentalShippingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranscontinentalShippingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aib/transcontinentalshippings/TranscontinentalShippingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aib.transcontinentalshippings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscontinentalShippingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
